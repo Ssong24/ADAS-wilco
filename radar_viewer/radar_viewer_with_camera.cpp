d0964f1e66aaf98d5d7fdd5f0c7aae1c65727184
Ssong24 <pub50@naver.com>
2025-06-16 17:17:55 +0300

    Update radar_viewer_with_camera.cpp

--- radar_viewer/radar_viewer_with_camera.cpp
+++ radar_viewer/radar_viewer_with_camera.cpp
@@ -143,6 +143,96 @@ float extract_vrel_lat(__u8 *data)
 }
 
 
+//Camera function
+
+string queryDisplayVid(string& displayOption) {
+    cout << "Display? (Y/N)" << endl;
+    cin >> displayOption;
+    return displayOption;
+}
+
+string querySaveVid(string& saveOption) {
+    string filename;
+    string extension = ".avi";
+    cout << "Save? (Y/N)" << endl;
+    cin >> saveOption;
+    if (saveOption == "Y") {
+        cout << "Filename:" << endl;
+        cin >> filename;
+        filename = filename + extension;
+        cout << "Saving the video to "
+             << filename << endl;
+    }
+    else if (saveOption == "N") {
+        cout << "Not saving the video." << endl;
+    }
+    return filename;
+}
+
+int displayNoSave(int(*point)[2], int row) {
+
+    VideoCapture cap(1);
+    if (!cap.isOpened()) {
+        cout << "Error opening video stream." << endl;
+        return -1;
+    }
+    else {       
+        while(1) {
+            Mat frame;
+            cap >> frame;
+            if (frame.empty())
+                break;
+	    
+	
+            for (int i=0;i<row;i++){
+		drawMarker(frame, Point2d(point[i][0], point[i][1]) , Scalar(0, 255, 0), MARKER_CROSS, 10, 1, 8);
+	    }
+
+            imshow("Frame", frame);
+            char c = (char)waitKey(1);
+            if (c == 27)
+                break ;
+        }
+        cap.release();
+        return 0;
+    }
+}
+
+int displaySave(string filename, int(*point)[2], int row) {
+    VideoCapture cap(1);
+    if (!cap.isOpened()) {
+        cout << "Error opening video stream." << endl;
+        return -1;
+    }
+    else {
+        int frame_width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+        int frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+        VideoWriter video(filename ,CV_FOURCC('M','J','P','G'), 10,
+                          Size(frame_width,frame_height));
+        while(1) {
+            Mat frame;
+            cap >> frame;
+            if (frame.empty())
+                break;
+	    
+            for (int i=0;i<row;i++){
+		drawMarker(frame, Point2d(point[i][0], point[i][1]) , Scalar(0, 255, 0), MARKER_CROSS, 10, 1, 8);
+	    }
+
+	    
+            video.write(frame);
+            imshow("Frame", frame);
+            char c = (char)waitKey(1);
+            if (c == 27)
+                break ;
+        }
+        cap.release();
+        video.release();
+        return 0;
+    }
+}
+
 
 using namespace std;
 using namespace cv;
@@ -306,6 +396,7 @@ int main(void)
 
 	close_port();
         cap.release();
+        video.release();
 
 	return 0;
 }
